2fe5ce9a3baae6a9cbce13b13304d81c130b2742
Ophiuqi <mycode@emailplus.org>
2025-12-09 16:08:02 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
-  title: 'Ezen.Work',
-  email: 'info@ezen.work',
+  title: 'Sink',
+  email: 'sink.cool@miantiao.me',
   github: 'https://github.com/miantiao-me/sink',
   twitter: 'https://sink.cool/kai',
   telegram: 'https://sink.cool/telegram',
   mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://ezen.work',
-  description: 'A simple short link generator for OutdoorRouter and EzenNetwork. Powered by Sink and operates securely on CloudFlare.',
+  blog: 'https://sink.cool/blog',
+  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
